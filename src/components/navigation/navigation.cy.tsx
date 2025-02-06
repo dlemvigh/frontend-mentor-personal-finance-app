65f9b3d974bf097413be9cfb22699ec639267985
David Lemvigh <dlemvigh@gmail.com>
2025-02-06 20:32:35 +0100

    nav icons wip

--- src/components/navigation/navigation.cy.tsx
+++ src/components/navigation/navigation.cy.tsx
@@ -2,9 +2,20 @@ import React from 'react'
 import { Navigation } from './navigation'
 
 describe('<Navigation />', () => {
-  it('renders', () => {
+  it('desktop view', () => {
     // see: https://on.cypress.io/mounting-react
     cy.viewport(1440, 1080)
+    cy.mount(<Navigation />)
+  })
+  it('tablet view', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.viewport(768, 1024)
+    cy.mount(<Navigation />)
+  })
+
+  it('mobile view', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.viewport(375, 750)
     cy.mount(<Navigation />)
   })
 })
