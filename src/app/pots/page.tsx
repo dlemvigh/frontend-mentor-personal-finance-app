805fe5e54fa4913ccd929fdd7740a0fc64f46d3c
David Lemvigh <dlemvigh@gmail.com>
2025-02-07 12:23:28 +0100

    Fix pot header row

--- src/app/pots/page.tsx
+++ src/app/pots/page.tsx
@@ -1,15 +1,20 @@
 import { PotsList } from "@/components/pots/pots-list";
+import { Button } from "@/components/ui/button/button";
 import { getPots } from "@/data/api";
+import styles from "./page.module.css";
 
 export default async function PotsPage() {
+  const data = await getPots();
 
-    const data = await getPots();
-
-    return (
-      <>
+  return (
+    <>
+      <div className={styles["header-row"]}>
         <h1>Pots</h1>
-        <PotsList pots={data} />
-      </>
-    );
-  }
-  
+        <Button type="button" variant="primary">
+          + Add New Pot
+        </Button>
+      </div>
+      <PotsList pots={data} />
+    </>
+  );
+}
