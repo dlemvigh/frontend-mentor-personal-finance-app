0411ee8e5d89f8141fa82541cf6279b3a3a41bcc
David Lemvigh <dlemvigh@gmail.com>
2025-02-16 21:31:20 +0100

    Refactor select

--- src/components/ui/select/select.tsx
+++ src/components/ui/select/select.tsx
@@ -1,45 +1,63 @@
 import { ReactNode } from "react"
-import * as RadixSelect from "@radix-ui/react-select"
+import { Root, Trigger, Value, Icon, Portal, Content, Viewport, Item, ItemText, Separator as RadixSeparator } from "@radix-ui/react-select"
 import styles from "./select.module.css"
 import iconCaretDown from "@/assets/images/icon-caret-down.svg"
 import classNames from "classnames"
 import Image from "next/image"
 
-interface SelectProps {
+interface NativeSelectProps {
     placeholder?: string
     defaultValue?: string
     value?: string
     onChange?: (value: string) => void
     children: ReactNode
-    mobileTriggerIcon?: ReactNode
 }
 
-export function Select({ defaultValue, value, onChange, placeholder, mobileTriggerIcon, children } : SelectProps) {
-
+export function NativeSelect({ defaultValue, value, onChange, placeholder, children }: NativeSelectProps) {
     return (
-        <RadixSelect.Root defaultValue={defaultValue} value={value} onValueChange={onChange}>            
-            <RadixSelect.Trigger className={classNames(styles.trigger, "text-preset-4", !!mobileTriggerIcon && styles["trigger-mobile"])}>
-                <div className={classNames("mobile-only", styles["trigger-content-mobile"])}>
-                    {mobileTriggerIcon}
-                </div>
-                <div className={classNames(!!mobileTriggerIcon && "mobile-hidden", styles["trigger-content"])}>
-                    <RadixSelect.Value placeholder={placeholder} />
-                    <RadixSelect.Icon asChild>
-                        <Image src={iconCaretDown} alt="" />
-                    </RadixSelect.Icon>
-                </div>
-                
-            </RadixSelect.Trigger>
-            <RadixSelect.Portal>
-                <RadixSelect.Content className={styles.content} position="popper">
-                    <RadixSelect.Viewport>
+        <Root defaultValue={defaultValue} value={value} onValueChange={onChange}>
+            <Trigger className={classNames(styles.trigger, styles["trigger-content"], "text-preset-4")}>
+                <Value placeholder={placeholder} />
+                <Icon asChild>
+                    <Image src={iconCaretDown} alt="" />
+                </Icon>
+            </Trigger>
+            <Portal>
+                <Content className={styles.content} position="popper">
+                    <Viewport style={{ overflow: "initial" }}>
                         {children}
-                    </RadixSelect.Viewport>
-                </RadixSelect.Content>
-            </RadixSelect.Portal>
-        </RadixSelect.Root>
+                    </Viewport>
+                </Content>
+            </Portal>
+        </Root>
     )
 }
+
+interface IconSelectProps {
+    defaultValue?: string
+    value?: string
+    onChange?: (value: string) => void
+    children: ReactNode
+    icon: string
+}
+
+export function IconSelect({ defaultValue, value, onChange, icon, children }: IconSelectProps) {
+    return (
+        <Root defaultValue={defaultValue} value={value} onValueChange={onChange}>
+            <Trigger className={classNames(styles.trigger, styles["trigger-icon"])}>
+                <Image className={styles.icon} src={icon} alt="Sort by" />
+            </Trigger>
+            <Portal>
+                <Content className={styles.content} position="popper">
+                    <Viewport style={{ overflow: "initial" }}>
+                        {children}
+                    </Viewport>
+                </Content>
+            </Portal>
+        </Root>
+    )
+}
+
 
 interface OptionProps {
     value: string
@@ -48,12 +66,12 @@ interface OptionProps {
 
 export function Option({ value, children }: OptionProps) {
     return (
-        <RadixSelect.Item value={value} className={classNames(styles.item, "text-preset-4")}>
-            <RadixSelect.ItemText>{children}</RadixSelect.ItemText>
-        </RadixSelect.Item>
+        <Item value={value} className={classNames(styles.item, "text-preset-4")}>
+            <ItemText>{children}</ItemText>
+        </Item>
     )
 }
 
 export function Separator() {
-    return <RadixSelect.Separator className={styles.separator} />
+    return <RadixSeparator className={styles.separator} />
 }
