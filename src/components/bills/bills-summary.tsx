bbcbeb28558447ea40fe73dbeaa36544b183dc7a
David Lemvigh <dlemvigh@gmail.com>
2025-02-17 21:11:38 +0100

    Add BillsSummary component styles and enhance item display

--- src/components/bills/bills-summary.tsx
+++ src/components/bills/bills-summary.tsx
@@ -1,4 +1,6 @@
 import { RecurringBillsSummary } from "@/data/api";
+import styles from "./bills-summary.module.css"
+import classNames from "classnames";
 
 interface BillsSummaryProps {
     summary: RecurringBillsSummary
@@ -7,14 +9,15 @@ interface BillsSummaryProps {
 export function BillsSummary({ summary }: BillsSummaryProps) {
     
     return (
-        <div>
-            Summary
-            <dl>
+        <div className={styles.card}>
+            <h2 className={`${styles.title} text-preset-3`}>Summary</h2>
+            
+            <dl className={styles.list}>
                 <BillsSummaryItem label="Paid Bills" count={summary.paid.count} total={summary.paid.total} />
-                <hr/>
+                <hr className={styles.divider} />
                 <BillsSummaryItem label="Total Upcomming" count={summary.upcoming.count} total={summary.upcoming.total} />
-                <hr/>
-                <BillsSummaryItem label="Due Soon" count={summary.due.count} total={summary.due.total} />
+                <hr className={styles.divider} />
+                <BillsSummaryItem label="Due Soon" count={summary.due.count} total={summary.due.total} due />
             </dl>
         </div>
     )
@@ -24,13 +27,14 @@ interface BillsSummaryItemProps {
     label: string
     count: number
     total: number    
+    due?: boolean
 }
 
-function BillsSummaryItem({ label, count, total }: BillsSummaryItemProps) {
+function BillsSummaryItem({ label, count, total, due }: BillsSummaryItemProps) {
     return (
         <>
-            <dt>{label}</dt>
-            <dd>{count} (${total})</dd>
+            <dt className={classNames("text-preset-5", styles["list-title"], due && styles["list-due"])}>{label}</dt>
+            <dd className={classNames("text-preset-5-bold", styles["list-data"], due && styles["list-due"])}>{count} (${total.toFixed(2)})</dd>
         </>
     )
 }
